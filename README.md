c77951ae8952244e7dbe6bc3dbdcd7f9f203a220
5kazo4nik <alhimov.oleg2012@yandex.ru>
2023-09-23 19:57:31 +0300

    feat: assemle prod files

--- README.md
+++
@@ -1,3 +0,0 @@
-# 5kazo4nik-JSFE2023Q1
-
-Rss-css-selectors - https://rolling-scopes-school.github.io/5kazo4nik-JSFE2023Q1/rss-css-selectors/
--- rss-css-selectors/src/scripts/creator/abstractCreator.ts
+++
@@ -1,9 +0,0 @@
-export abstract class ElementCreator {
-  protected abstract buildElement(): void;
-
-  protected abstract addStatus(tableElem?: HTMLElement): HTMLElement | void;
-
-  protected abstract appendElem(parent?: HTMLElement): void;
-
-  public abstract getElement(): HTMLElement;
-}
